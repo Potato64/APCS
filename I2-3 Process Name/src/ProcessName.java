415baae95e1fecfa80a33cc8ce40f9578793e868
Potato64 <32688383+Potato64@users.noreply.github.com>
2018-02-15 19:01:20 -0800

    Added header for processName

--- I2-3 Process Name/src/ProcessName.java
+++ I2-3 Process Name/src/ProcessName.java
@@ -1,3 +1,16 @@
+/*
+Name: Ethan Duer
+Project Name: 12-3 Process Name
+File Name: ProcessName.java
+Purpose: To create a function that takes a scanner for the console, and gets the user's full name.
+The program then prints the name in reverse order
+Pseudocode: Pass a new scanner with the console as a parameter to a function.
+The function gets the user's name and returns it to main. A for loop prints the string character
+by character in reverse.
+Maintenance Log:
+2/13/18: Project created
+*/
+
 import java.util.Scanner;
 
 public class ProcessName {
